a86806d842d4edbbe66c22faef042f2e048ea065
ldemaz <3401881+ldemaz@users.noreply.github.com>
2018-09-20 17:34:10 -0400

    More edits to planet downloader

--- materials/code/python/planet_downloader.py
+++ materials/code/python/planet_downloader.py
@@ -8,6 +8,7 @@ from geojson import Polygon, MultiPolygon
 import geojson
 import json
 import logging
+import datetime
 
 
 # read configuration
@@ -37,16 +38,15 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.basicConfig(format = '%(message)s', datefmt = '%m-%d %H:%M')
 
-# print(api_key)
-# print(image_path)
+download = False
+cloud_filter = False
 
 # pclient init
 pclient = PClientV1(api_key, config)
-# print(pclient)
 
-# build a valid dt string from a month number
-def dt_construct(month, day = 1, year = 2018, t = "00:00:00.000Z"):
-    return "{}-{:02d}-{:02d}T{}".format(year, month, day, t)
+# # build a valid dt string from a month number
+# def dt_construct(month, day = 1, year = 2018, t = "00:00:00.000Z"):
+#     return "{}-{:02d}-{:02d}T{}".format(year, month, day, t)
 
 aoi = GeoUtils.define_aoi(x, y, cellSize)  # aoi by a cell grid x, y
 # print(aoi)
@@ -57,44 +57,71 @@ output_file = ''
 output_localfile = ''
 # tms_uri = ''
 
-start_date = dt_construct(month = mo_start, day = day_start, year = yr_start)
-end_date = dt_construct(month = mo_end, day = day_end, year = yr_end)
-# print(start_date)
-# print(end_date)
-planet_filters = pclient.set_filters_sr(aoi, start_date, end_date)
-# print(planet_filters)
-res = pclient.request_intersecting_scenes(planet_filters)
-# print(res)
+# start_date = dt_construct(month = mo_start, day = day_start, year = yr_start)
+# end_date = dt_construct(month = mo_end, day = day_end, year = yr_end)
+# step = datetime.timedelta(days=1)
+start = datetime.datetime(yr_start, mo_start, day_start, 0, 0, 0)
+# start2 = datetime.datetime(yr_start, mo_start, day_start, 23, 59, 59)
+end = datetime.datetime(yr_end, mo_end, day_end, 0, 0, 0, 0)
+# step = datetime.timedelta(days=1)
+start_date = start.strftime('%Y-%m-%dT%H:%M:%SZ')
+end_date = end.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-# pick up scene id and its geometry
+# 
+planet_filters = pclient.set_filters_sr(aoi, start_date, end_date)
+# # print(planet_filters)
+res = pclient.request_intersecting_scenes(planet_filters)
+# # print(res)
+# 
+# # pick up scene id and its geometry
 for item in res.items_iter(pclient.maximgs):
     # each item is a GeoJSON feature
      geom = shape(geojson.loads(json.dumps(item["geometry"])))
      scene_id = item["id"]
-     print(scene_id)
-     # print(geom)
+     print("Matching scene ID: " + scene_id)
      
-     # output_file = pclient.download_localfs_analytic_sr(scene_id)
-
-     # cleanup local catalog to remove previous iterations files
-     # pclient.cleanup_catalog()
-     # # activation & download
-     # # it should be sync, to allow async check of neighbours
-     # output_localfile, output_file = pclient.download_localfs_s3(scene_id, season = season_type)
-     # 
-     # bbox_local = GeoUtils.define_BoundingBox(x, y, cellSize)
-     # print(bbox_local)
-     # nodata percentage
-     # nodata_perc = nodata_stats(output_file, bbox_local)
-     # use custom cloud detection function to calculate clouds and shadows
-     # cloud_perc, shadow_perc = cloud_stats(output_file, bbox_local, cloud_config)
-     # # check if cell grid is good enough
-     # if (cloud_perc <= pclient.max_clouds and nodata_perc <= pclient.max_nodata):
-     #     break
-     # else: 
-     #     scene_id = ''
-         
+     if download:
+        print("...Downloading " + scene_id)
+        # output_file = pclient.download_localfs_analytic_sr(scene_id)
+     
+     if cloud_filter:
+        bbox_local = GeoUtils.define_BoundingBox(x, y, cellSize)
+        # print(bbox_local)
+        
+       # nodata percentage
+       # nodata_perc = nodata_stats(output_file, bbox_local)
+       
+       # use custom cloud detection function to calculate clouds and shadows
+       # cloud_perc, shadow_perc = cloud_stats(output_file, bbox_local, cloud_config)
+       # check if cell grid is good enough
+       # if (cloud_perc <= pclient.max_clouds and nodata_perc <= pclient.max_nodata):
+           # break
+     # else:
+           # scene_id = ''
+                                      
                                         
-                                        
-                                        
+# # For each day, find the corresponding scene ids that pass filters
+# while start <= end:
+#     # print(start)
+#     s  = start.strftime('%Y-%m-%dT%H:%M:%SZ')
+#     s2  = start2.strftime('%Y-%m-%dT%H:%M:%SZ')
+#     # print(s)
+#     # print(s2)
+#     start += step
+#     start2 += step
+#     planet_filters = pclient.set_filters_sr(aoi, s, s2)
+#     res = pclient.request_intersecting_scenes(planet_filters)
+#     
+#     print("Querying scenes for " + start.strftime('%Y-%m-%d'))
+#     print(str(res))
+    
+    # loop through all scenes for day
+    # for item in res.items_iter(pclient.maximgs):
+    #     geom = shape(geojson.loads(json.dumps(item["geometry"])))
+    #     scene_id = item["id"]
+    #     print(item)
+        # if scene_id:
+        #     print(scene_id)
+        # else: 
+        #     print("...no scene")
                                         
